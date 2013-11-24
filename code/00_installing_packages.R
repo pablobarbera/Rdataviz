da46c7fbf1685b3f64afccdedb38933d0f3eb928
pablobarbera <pablobarbera@gmail.com>
2013-11-24 11:15:50 -0500

    updated Rfacebook installation

--- code/00_installing_packages.R
+++ code/00_installing_packages.R
@@ -3,24 +3,20 @@
 doInstall <- TRUE  # Change to FALSE if you don't want packages installed.
 
 toInstall <- c(
-    "devtools", ## library to install packages from github
     "ggplot2", "scales", "gridExtra", "hexbin", ## gplot2 and extensions
-    "gdata", ## library to open excel files
-    "igraph", ## library to create network objects
+    "gdata", ## package to open excel files
+    "igraph", ## package to create network objects
     "cshapes", 
     "sp", "maps", "maptools", ## map utilities
     "RColorBrewer", ## additional color pallets
     "XML", ## scraping tool
-    "reshape", ## data manipulation library
-    "jpeg" ## library to open jpeg images from R
+    "reshape", ## data manipulation package
+    "jpeg", ## package to open jpeg images from R
+    "Rfacebook" ## my package to capture Facebook data
     )
 
 if(doInstall){
-
     install.packages(toInstall, repos = "http://cran.r-project.org")
-
-    library(devtools)
-    install_github("Rfacebook", "pablobarbera")
 }
 
 
